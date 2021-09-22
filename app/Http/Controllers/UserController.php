f08a28921b4352edba4ab0fdc23b1a365dfd728f
sajjaddarre76 <sajjaddarre76@gmail.com>
2021-09-22 04:37:38 -0700

    login done

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    function login(Request $req){
+        $user = User::where('email', $req->email)->first();
+        if($user || Hash::check($req->password, $user->password)){
+            $req->session()->put('user', $user);
+            return redirect('/');
+        }else{
+            return 'Username or password not matched';
+        }
+    }
+}
