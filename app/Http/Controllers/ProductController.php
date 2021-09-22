6b15080405df33aebf1044108c71b7a21a79e2ef
sajjaddarre76 <sajjaddarre76@gmail.com>
2021-09-22 06:53:36 -0700

    cartItems count

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Cart;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class ProductController extends Controller
 {
@@ -35,5 +36,11 @@ class ProductController extends Controller
         }else{
             return redirect('/login');
         }
+    }
+
+    static public function cartItem()
+    {
+        $user_id = Session::get('user')['id'];
+        return Cart::where('user_id', $user_id)->count();
     }
 }
